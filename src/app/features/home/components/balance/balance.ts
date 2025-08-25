ad615a31ffc4dbd39e34b4f21175e1b4bf051bc9
victorduarte31 <devvictorduarte@gmail.com>
2025-08-25 10:08:07 -0300

    Adiciona componente Balance para calcular e exibir ganhos, gastos e saldo atual dinamicamente, além de ajustar estrutura do módulo Home.

---
+++ src/app/features/home/components/balance/balance.ts
@@ -0,0 +1,32 @@
+import {Component, computed, input} from '@angular/core';
+import {BalanceCard} from "./components/balance-card/balance-card";
+
+@Component({
+  selector: 'app-balance',
+  imports: [
+    BalanceCard
+  ],
+  templateUrl: './balance.html',
+  styleUrl: './balance.scss'
+})
+export class Balance {
+
+  transactions = input.required<{ value: number, type: string }[]>();
+
+  totalIncomes = computed(() => {
+    return this.transactions()
+      .filter(item => item.type === 'income')
+      .reduce((total, item) => total + item.value, 0);
+  })
+
+  totalOutcomes = computed(() => {
+    return this.transactions()
+      .filter(item => item.type === 'outcome')
+      .reduce((total, item) => total + item.value, 0);
+  })
+
+  balance = computed(() => {
+    return this.totalIncomes() - this.totalOutcomes();
+
+  })
+}
--- src/app/features/home/home.ts
+++ src/app/features/home/home.ts
@@ -1,14 +1,21 @@
-import { Component } from '@angular/core';
-import {BalanceCard} from './components/balance-card/balance-card';
+import {Component, signal} from '@angular/core';
+import {Balance} from './components/balance/balance';
 
 @Component({
   selector: 'app-home',
   imports: [
-    BalanceCard
+    Balance
   ],
   templateUrl: './home.html',
   styleUrl: './home.scss'
 })
 export class Home {
+
+  transaction = signal([
+    {value: 500, type: 'income'},
+    {value: 150, type: 'outcome'},
+    {value: 200, type: 'outcome'},
+    {value: 100, type: 'income'},
+  ]);
 
 }
